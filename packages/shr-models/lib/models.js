b2a05963b66b7e14a2f9903f6fc98de69427d596
troycrews <troycrews@users.noreply.github.com>
2016-11-07 10:59:51 -0500

    Merge pull request #1 from standardhealth/updated_text_format
    Grammar v2 Plus

--- packages/shr-models/lib/models.js
+++ packages/shr-models/lib/models.js
@@ -1,55 +1,117 @@
 class Namespace {
   constructor(namespace) {
     this._namespace = namespace; // string
-    this._elementMap = {}; // obj[string]=DataElement|Entry
+    this._sections = []; // Section[]
+    this._definitionIdentifiers = []; // Identifier[] (keeping track in an array allows us to preserve order)
+    this._definitionMap = {}; // obj[string]=DataElement|Group
   }
 
   get namespace() { return this._namespace; }
 
-  get elements() { return Object.keys(this._elementMap).map(key => this._elementMap[key]); }
-  addElement(element) {
-    this._elementMap[element.identifier.name] = element;
+  get sections() { return this._sections; }
+  addSection(section) {
+    this._sections.push(section);
+  }
+
+  // a definition might be a DataElement or a Group
+  get definitions() { return this._definitionIdentifiers.map(id => this._definitionMap[id]); }
+  addDefinition(definition) {
+    if (typeof this.lookup(definition.identifier.name) === 'undefined') {
+      this._definitionIdentifiers.push(definition.identifier.name);
+    }
+    this._definitionMap[definition.identifier.name] = definition;
+  }
+
+  lookup(name) {
+    return this._definitionMap[name];
   }
 }
 
-class DataElement {
+class Identifiable {
   constructor(identifier) {
+    if (this.constructor === Identifiable) {
+      throw new TypeError('Abstract class "Identifiable" cannot be instantiated directly.');
+    }
+
     this._identifier = identifier; // Identifier
-    this._answers = []; // Identifier[]
-    this._components = []; // QuantifiedIdentifier[]
   }
 
   get identifier() { return this._identifier; }
+}
 
+class Section extends Identifiable {
+  constructor(identifier) {
+    super(identifier);
+    this._entries = []; // QuantifiedValue[]
+  }
+
+  // an entry must be a QuantifiedValue referencing a DataElement or Group (no primitives)
+  get entries() { return this._entries; }
+  addEntry(quantifiedValue) {
+    this._entries.push(quantifiedValue);
+  }
+}
+
+class BaseElement extends Identifiable {
+  constructor(identifier) {
+    super(identifier);
+    this._concepts = [];
+    if (this.constructor === BaseElement) {
+      throw new TypeError('Abstract class "BaseElement" cannot be instantiated directly.');
+    }
+  }
+
+  // concepts are an array of Concept
+  get concepts() { return this._concepts; }
+  set concepts(concepts) {
+    this._concepts = concepts;
+  }
+  addConcept(concept) {
+    this._concepts.push(concept);
+  }
+
+  // a description is a string
   get description() { return this._description; }
   set description(description) {
     assertNoOverwrite(this._description, this, 'description', description);
     this._description = description;
   }
+}
 
-  get answers() { return this._answers; }
-  addAnswer(identifier) {
-        // TODO: enforce that this must be an Identifier?
-    this._answers.push(identifier);
+class DataElement extends BaseElement {
+  constructor(identifier) {
+    super(identifier);
   }
 
-  get valueset() { return this._valueset; }
-  set valueset(valueset) {
-    assertNoOverwrite(this._valueset, this, 'valueset', valueset);
-    this._valueset = valueset;
-  }
-
-  get components() { return this._components; }
-  addComponent(quantifiedIdentifier) {
-        // TODO: enforce that this must be a QuantifiedIdentifier?
-    this._components.push(quantifiedIdentifier);
+  // a value might be a Value, subclass of Value, QuantifiedValue, or OrValues
+  get value() { return this._value; }
+  set value(value) {
+    assertNoOverwrite(this._value, this, 'value', value);
+    this._value = value;
   }
 }
 
-class Entry extends DataElement {
-  constructor(namespace, name) {
-    super(namespace, name);
+class Group extends BaseElement {
+  constructor(identifier) {
+    super(identifier);
+    this._elements = []; // QuantifiedValue[]
   }
+
+  // an element must be a QuantifiedValue referencing a DataElement or Group (no primitives)
+  get elements() { return this._elements; }
+  addElement(quantifiedValue) {
+    this._elements.push(quantifiedValue);
+  }
+}
+
+class Concept {
+  constructor(codesystem, code) {
+    this._codesystem = codesystem;
+    this._code = code;
+  }
+
+  get codesystem() { return this._codesystem; }
+  get code() { return this._code; }
 }
 
 class Identifier {
@@ -73,18 +135,55 @@ class PrimitiveIdentifier extends Identifier {
   }
 }
 
-class QuantifiedIdentifier extends Identifier {
-  constructor(identifier, min, max) {
-    super(identifier.namespace, identifier.name);
+class Value extends Identifiable {
+  constructor(identifier) {
+    super(identifier);
+  }
+}
+
+// CodeValue assumes identifier PrimitiveIdentifier('code'), so it only takes a valueset
+class CodeValue extends Value {
+  constructor(valueset) {
+    super(new PrimitiveIdentifier('code'));
+    this._valueset = valueset; // string in url form
+  }
+
+  get valueset() { return this._valueset; }
+}
+
+class RefValue extends Value {
+  constructor(identifier) {
+    super(identifier);
+  }
+}
+
+class QuantifiedValue {
+  constructor(value, min, max) {
+    this._value = value; // Value|OrValues
     this._min = min; // number
     this._max = max; // number|undefined
   }
 
+  get value() { return this._value; }
+  set value(value) {
+    this._value = value;
+  }
   get min() { return this._min; }
   get max() { return this._max; }
 
   isMaxUnbounded() {
     return typeof this._max === 'undefined' || this._max == null;
+  }
+}
+
+class OrValues {
+  constructor() {
+    this._values = []; // (Value|QuantifiedValue|OrValues)[]
+  }
+
+  get values() { return this._values; }
+  addValue(value) {
+    this._values.push(value);
   }
 }
 
@@ -94,8 +193,8 @@ const PRIMITIVES = ['boolean', 'integer', 'decimal', 'unsignedInt', 'positiveInt
 
 function assertNoOverwrite(property, element, propName, newValue) {
   if (typeof property !== 'undefined') {
-    console.warn(`WARNING: Overwriting ${element.fqn()}['${propName}']:\n\twas: ${property}\n\tnow: ${newValue}`);
+    console.warn(`WARNING: Overwriting ${element.identifier.fqn}['${propName}']:\n\twas: ${property}\n\tnow: ${newValue}`);
   }
 }
 
-module.exports = {Namespace, DataElement, Entry, Identifier, PrimitiveIdentifier, QuantifiedIdentifier, PRIMITIVE_NS, PRIMITIVES};
+module.exports = {Namespace, Section, DataElement, Group, Concept, Identifier, PrimitiveIdentifier, Value, CodeValue, RefValue, QuantifiedValue, OrValues, PRIMITIVE_NS, PRIMITIVES};
