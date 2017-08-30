e351960989b1400b9a3767f5282ea06b9b082570
John Gibson <jgibson@mitre.org>
2017-08-30 14:52:11 -0400

    Added support for choice, codes, groups, and references.

--- packages/shr-json-schema-export/lib/export.js
+++ packages/shr-json-schema-export/lib/export.js
@@ -38,92 +38,34 @@ function namespaceToSchema(ns, dataElements, grammarVersions) {
       type: 'object',
       properties: {}
     };
-    let required = [];
+
+    let requiredProperties = [];
     if (def.value) {
-      required.push('Value');
-      const value = schemaDef.properties.Value = {};
-      // TODO: Is this really the best way to identify a type in ES6?
-      logger.debug('Value type: %s', def.value.constructor.name);
-      if (def.value.constructor.name === 'ChoiceValue') {
-        logger.error('Unsupported ChoiceValue');
-      } else if (def.value.constructor.name === 'RefValue') {
-        logger.error('Unsupported RefValue');
-      } else if (def.value.constructor.name === 'IdentifiableValue') {
-        const id = def.value.effectiveIdentifier;
-        if (id.isPrimitive) {
-          switch (id.name) {
-            case 'boolean':
-            case 'string':
-            case 'integer':
-              value.type = id.name;
-              break;
-            case 'unsignedInt':
-              value.type = 'integer';
-              value.minimum = 0;
-              break;
-            case 'positiveInt':
-              value.type = 'integer';
-              value.minimum = 1;
-              break;
-            case 'decimal':
-              value.type = 'number';
-              break;
-            case 'uri':
-              value.type = 'string';
-              value.format = 'uri';
-              break;
-            case 'base64Binary':
-              value.type = 'string';
-              break;
-            case 'dateTime':
-              value.type = 'string';
-              value.format = 'dateTime';
-              break;
-            case 'instant':
-            case 'date':
-            case 'time':
-            case 'code':
-            case 'oid':
-            case 'id':
-            case 'markdown':
-            case 'xhtml':
-              value.type = 'string';
-              break;
-          }
-        } else {
-          if (id.namespace === ns.namespace) {
-            value['$ref'] = '#' + def.value.identifier.name;
-          } else {
-            value['$ref'] = "https://standardhealthrecord.org/test/" + def.value.identifier.namespace.replace(/\./g, '/') + '#definitions/' + def.value.identifier.name;
-          }
-        }
-      } else if (def.value.constructor.name === 'TBD') {
-        logger.error('Unsupported TBD');
-      } else if (def.value.constructor.name === 'IncompleteValue') {
-        logger.error('Unsupported Incomplete');
-      } else {
-        logger.error('Unknown type for value "%s"', def.value.constructor.name);
+      let {value, required} = convertDefinition(def.value, ns);
+      if (!required) {
+        logger.error('Internal error: Value was not required?', required);
       }
+      requiredProperties.push('Value');
+      schemaDef.properties.Value = value;
     }
     if (def.fields.length) {
-      for (const field in def.fields) {
+      for (const field of def.fields) {
+        if (field.constructor.name === 'ChoiceValue') {
+          logger.error('ERROR: Ignoring field defined as a choice', field);
+          continue;
+        }
         if (field.identifier.name === 'Value') {
           logger.error('ERROR: Ignoring restricted field name: Value', field);
           continue;
         }
         const card = field.effectiveCard;
-        if (card.isList) {
-          logger.error('List cardinality is unsupported. Field: "%s"', field.identifier.name);
+        if (card.isZeroedOut) {
           continue;
-        } else if (card.min) {
-          required.push(field.identifier.name);
         }
-        if (field.value && field.value.identifier) {
-          if (field.value.identifier.namespace.namespace === ns) {
-            schemaDef.properties[field.identifier.name] = { '$ref': '#' + field.value.identifier.name};
-          } else {
-            schemaDef.properties[field.identifier.name] = { '$ref': "https://standardhealthrecord.org/test/" + field.value.identifier.namespace.replace(/\./g, '/') + '#' + field.value.identifier.name};
-          }
+        let {value, required} = convertDefinition(field, ns);
+        schemaDef.properties[field.identifier.name] = value;
+        if (required) {
+          requiredProperties.push(field.identifier.name);
         }
       }
     } else if (!def.value) {
@@ -134,14 +76,143 @@ function namespaceToSchema(ns, dataElements, grammarVersions) {
     if (def.concepts.length) {
       schemaDef.description += '\nConcepts: ' + def.concepts.map((c) => { return conceptToString(c); }).join(',');
     }
-    if (required.length) {
-      schemaDef.required = required;
+    if (requiredProperties.length) {
+      schemaDef.required = requiredProperties;
     }
 
     schema.definitions[def.identifier.name] = schemaDef;
   }
 
   return schema;
+}
+
+function convertDefinition(valueDef, enclosingNamespace) {
+  const retValue = {};
+  let value = retValue;
+  const card = valueDef.effectiveCard;
+  let required = false;
+  let isCode = false;
+  if (card.isList) {
+    retValue.type = 'array';
+    if (card.min) {
+      retValue.minItems = card.min;
+      required = true;
+    }
+    if (card.max) {
+      retValue.maxItems = card.max;
+    }
+    value = retValue.items = {};
+  } else if (card.min) {
+    required = true;
+  }
+
+  // TODO: Is this really the best way to identify a type in ES6?
+  logger.debug('Value type: %s', valueDef.constructor.name);
+  if (valueDef.constructor.name === 'ChoiceValue') {
+    value.oneOf = [];
+    for (const option of valueDef.options) {
+      const { value: childValue } = convertDefinition(option, enclosingNamespace);
+      value.oneOf.push(childValue);
+    }
+  } else if (valueDef.constructor.name === 'RefValue') {
+    // TODO: Schema extension or base definition: make this a first-class object.
+    value.type = 'object';
+    value.properties = {
+      ShrId: { type: 'string' },
+      EntryType: { type: 'string', format: 'uri' },
+      EntryId: { type: 'string' }
+    };
+    value.required = ['ShrId', 'EntryType', 'EntryId'];
+  } else if (valueDef.constructor.name === 'IdentifiableValue') {
+    const id = valueDef.effectiveIdentifier;
+    if (id.isPrimitive) {
+      switch (id.name) {
+        case 'boolean':
+        case 'string':
+        case 'integer':
+          value.type = id.name;
+          break;
+        case 'unsignedInt':
+          value.type = 'integer';
+          value.minimum = 0;
+          break;
+        case 'positiveInt':
+          value.type = 'integer';
+          value.minimum = 1;
+          break;
+        case 'decimal':
+          value.type = 'number';
+          break;
+        case 'uri':
+          value.type = 'string';
+          value.format = 'uri';
+          break;
+        case 'base64Binary':
+          value.type = 'string';
+          break;
+        case 'dateTime':
+          value.type = 'string';
+          value.format = 'dateTime';
+          break;
+        case 'instant':
+        case 'date':
+        case 'time':
+        case 'code':
+          value.type = 'string';
+          isCode = true;
+          break;
+        case 'oid':
+        case 'id':
+        case 'markdown':
+        case 'xhtml':
+          value.type = 'string';
+          break;
+      }
+    } else {
+      if (id.namespace === enclosingNamespace.namespace) {
+        value['$ref'] = '#' + valueDef.identifier.name;
+      } else {
+        value['$ref'] = "https://standardhealthrecord.org/test/" + valueDef.identifier.namespace.replace(/\./g, '/') + '#definitions/' + valueDef.identifier.name;
+      }
+    }
+  } else if (valueDef.constructor.name === 'TBD') {
+    logger.error('Unsupported TBD');
+  } else if (valueDef.constructor.name === 'IncompleteValue') {
+    logger.error('Unsupported Incomplete');
+  } else {
+    logger.error('Unknown type for value "%s"', valueDef.constructor.name);
+  }
+
+  const description = [];
+  for (const constraint of valueDef.constraints) {
+    if (constraint instanceof ValueSetConstraint) {
+      if (!isCode) {
+        logger.error('ERROR: valueset constraint %s was applied to a non-coding type %s', valueDef, constraint);
+        continue;
+      }
+      description.push('ValueSet: ' + constraint.valueSet + ', ' + constraint.bindingStrength);
+    } else if (constraint instanceof CodeConstraint) {
+      if (!isCode) {
+        logger.error('ERROR: code constraint %s was applied to a non-coding type %s', valueDef, constraint);
+        continue;
+      }
+      description.push('Code: ' + constraint.code);
+    } else if (constraint instanceof IncludesCodeConstraint) {
+      if (!(isCode && card.isList)) {
+        logger.error('ERROR: includes code constraint %s was applied to a non-coding array type %s', valueDef, constraint);
+        continue;
+      }
+      description.push('Includes Code: ' + constraint.code);
+    } else {
+      logger.info('WARNING: Constraint not yet implemented', constraint);
+    }
+  }
+
+  if (description.length) {
+    retValue.description = 'Constraints: ' + description.join('\n');
+  }
+
+  return {value: retValue, required};
 }
 
 function conceptToString(concept) {
